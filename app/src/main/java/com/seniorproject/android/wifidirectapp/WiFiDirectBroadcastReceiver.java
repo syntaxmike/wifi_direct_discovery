13109e160c0614f4f0cde0d2917cbe213951994d
michael <mocegue8@calstatela.com>
2017-09-10 22:13:04 -0700

    Wifi Direct Discovery App Sample

---
+++ app/src/main/java/com/seniorproject/android/wifidirectapp/WiFiDirectBroadcastReceiver.java
@@ -0,0 +1,67 @@
+package com.seniorproject.android.wifidirectapp;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.NetworkInfo;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
+import android.net.wifi.p2p.WifiP2pManager.ConnectionInfoListener;
+import android.util.Log;
+
+/**
+ * Created by Syntax Mike on 9/10/2017.
+ */
+
+public class WiFiDirectBroadcastReceiver extends BroadcastReceiver{
+
+    private WifiP2pManager manager;
+    private WifiP2pManager.Channel channel;
+    private Activity activity;
+    /**
+     * @param manager WifiP2pManager system service
+     * @param channel Wifi p2p channel
+     * @param activity activity associated with the receiver
+     */
+    public WiFiDirectBroadcastReceiver(WifiP2pManager manager, WifiP2pManager.Channel channel,
+                                       Activity activity) {
+        super();
+        this.manager = manager;
+        this.channel = channel;
+        this.activity = activity;
+    }
+    /*
+     * (non-Javadoc)
+     * @see android.content.BroadcastReceiver#onReceive(android.content.Context,
+     * android.content.Intent)
+     */
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        Log.d(WiFiDirectMain.TAG, action);
+        if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+            if (manager == null) {
+                return;
+            }
+            NetworkInfo networkInfo = (NetworkInfo) intent
+                    .getParcelableExtra(WifiP2pManager.EXTRA_NETWORK_INFO);
+            if (networkInfo.isConnected()) {
+                // we are connected with the other device, request connection
+                // info to find group owner IP
+                Log.d(WiFiDirectMain.TAG,
+                        "Connected to p2p network. Requesting network details");
+                manager.requestConnectionInfo(channel,
+                        (WifiP2pManager.ConnectionInfoListener) activity);
+            } else {
+                // It's a disconnect
+            }
+        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION
+                .equals(action)) {
+            WifiP2pDevice device = (WifiP2pDevice) intent
+                    .getParcelableExtra(WifiP2pManager.EXTRA_WIFI_P2P_DEVICE);
+            Log.d(WiFiDirectMain.TAG, "Device status -" + device.status);
+        }
+    }
+}
---
+++ app/src/main/java/com/seniorproject/android/wifidirectapp/WiFiP2pService.java
@@ -0,0 +1,14 @@
+package com.seniorproject.android.wifidirectapp;
+
+
+import android.net.wifi.p2p.WifiP2pDevice;
+
+/**
+ * Created by Syntax Mike on 9/10/2017.
+ */
+
+public class WiFiP2pService {
+    WifiP2pDevice device;
+    String instanceName = null;
+    String serviceRegistrationType = null;
+}
